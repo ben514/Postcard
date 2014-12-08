90da6e0988412a971b9080b31c4e1c78b7b22682
ben514 <Opportunity@Bens-iMac.local>
2014-12-08 00:54:23 -0800

    Initial Commit

---
+++ Supercar Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Supercar Postcard
+//
+//  Created by Ben Choi on 2014. 12. 8..
+//  Copyright (c) 2014년 Jamin Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Supercar PostcardTests/Supercar_PostcardTests.swift
@@ -0,0 +1,36 @@
+//
+//  Supercar_PostcardTests.swift
+//  Supercar PostcardTests
+//
+//  Created by Ben Choi on 2014. 12. 8..
+//  Copyright (c) 2014년 Jamin Apps. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Supercar_PostcardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
